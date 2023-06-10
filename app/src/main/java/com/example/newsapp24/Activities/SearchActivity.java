a887ee2588d112a58becd487b63441870e4152e0
RobyC1012 <roby1012@yahoo.com>
2023-06-10 13:46:34 +0300

    splash screen and prepare search and settings

---
+++ app/src/main/java/com/example/newsapp24/Activities/SearchActivity.java
@@ -0,0 +1,16 @@
+package com.example.newsapp24.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.newsapp24.R;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
---
+++ app/src/main/java/com/example/newsapp24/Activities/SplashScreenActivity.java
@@ -0,0 +1,16 @@
+package com.example.newsapp24.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.newsapp24.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
